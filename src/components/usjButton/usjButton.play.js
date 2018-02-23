8ad750176c8c1e92f36f279ff0b287f7d3ce7d05
Marco Leong <marcogood411@gmail.com>
2018-02-23 17:07:52 +0800

    Add .usj-inverse background in usj-icon

--- src/components/usjButton/usjButton.play.js
+++ src/components/usjButton/usjButton.play.js
@@ -1,6 +1,7 @@
 import { play } from 'vue-play'
 
 import usjButton from './usjButton.vue'
+import usjIcon from '../usjIcon/usjIcon.vue'
 
 play('USJ Button')
   .add('basic', {
@@ -11,5 +12,16 @@ play('USJ Button')
 <usj-button :disabled="true">Default Disabled button</usj-button>
 <usj-button class="usj-button--raised usj-button--accent" @click="$log('Clicked !')">Click me</usj-button>
 </div>
+    `
+  })
+  .add('icon button', {
+    components: { usjIcon, usjButton },
+    template: `
+      <div>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <p> Default <usj-button class="usj-icon-button"><usj-icon>home</usj-icon></usj-button></p>
+      <p> Background <usj-button class="usj-icon-button usj-icon-background"><usj-icon>home</usj-icon></usj-button></p>
+      <p> Inverse <usj-button class="usj-icon-button usj-inverse"><usj-icon>home</usj-icon></usj-button></p>
+      </div>
     `
   })
