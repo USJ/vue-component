6ad6b5300eeef15e2e54ddde899249d955a2fd80
Rivka Fong <rivka1386@hotmail.com>
2018-02-23 18:18:41 +0800

    embed css into component

--- src/components/usjFile/usjFile.vue
+++ src/components/usjFile/usjFile.vue
@@ -99,6 +99,24 @@ export default {
 }
 </script>
 
-<style lang="scss" src="./usjFile.scss">
+<style lang="scss">
+  .usj-file {
+    display: flex;
+    flex: 1;
 
+    input[type="file"] {
+      width: 1px;
+      height: 1px;
+      margin: -1px;
+      padding: 0;
+      overflow: hidden;
+      position: absolute;
+      clip: rect(0 0 0 0);
+      border: 0;
+    }
+
+    .md-icon {
+      cursor: pointer;
+    }
+  }
 </style>
