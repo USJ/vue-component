c05ac42145797163fd31e917deb321bff3b19639
Marco Leong <marcogood411@gmail.com>
2018-02-26 11:55:35 +0800

    Update usjSpinner to use .vue

--- src/components/usjSpinner/index.js
+++ src/components/usjSpinner/index.js
@@ -1,38 +1,5 @@
-require('./usjSpinner.css')
-
-function range(begin, end) {
-  return Array.from(new Array(end), (val, index) => index + begin)
-}
-
-const usjSpinner = {
-  render: function(h) {
-    let children = []
-    range(1, 13).forEach(i => {
-      children.push(
-        h('div', {
-          class: `sk-circle${i} sk-child`
-        })
-      )
-    })
-
-    return h(
-      this.tag,
-      {
-        class: 'sk-circle usj-spinner'
-      },
-      children
-    )
-  },
-  props: {
-    tag: {
-      type: String,
-      default: 'div'
-    }
-  }
-}
+import usjSpinner from './usjSpinner.vue'
 
 export default function install(Vue) {
   Vue.component('usj-spinner', usjSpinner)
 }
-
-export { usjSpinner }
